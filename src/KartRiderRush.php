ed436cf281bb914c9069484649b84b79d615ff1e
Neptune <neomunjeong@gmail.com>
2023-12-16 22:54:19 +0900

    feature: composer.json 추가

--- src/KartRiderRush.php
+++ src/KartRiderRush.php
@@ -6,5 +6,31 @@ namespace Neptunerere\PhpKartRiderRush;
 
 class KartRiderRush
 {
-    
+    /**
+     * @var Configuration
+     */
+    protected $configuration;
+
+    /**
+     * @param Configuration $configuration
+     */
+    public function __construct(Configuration $configuration)
+    {
+        $this->configuration = $configuration;
+    }
+
+    public function getConfiguration()
+    {
+        return $this->configuration;
+    }
+
+    /**
+     * @param Configuration $configuration
+     * @return self
+     */
+    public function setConfiguration($configuration)
+    {
+        $this->configuration = $configuration;
+        return $this;
+    }
 }
